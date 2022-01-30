294f3faa24136ae04460791d7e1e5756f22ba417
Mysochenko Yuriy <yuriy.mysochenko@gmail.com>
2022-01-30 14:17:05 +0200

    add editor

---
+++ app/src/main/java/space/taran/arkretouch/FilterItem.kt
@@ -0,0 +1,6 @@
+package space.taran.arkretouch
+
+import android.graphics.Bitmap
+import com.zomato.photofilters.imageprocessors.Filter
+
+data class FilterItem(var bitmap: Bitmap, val filter: Filter)
---
+++ app/src/main/java/space/taran/arkretouch/FilterThumbnailsManager.kt
@@ -0,0 +1,26 @@
+package space.taran.arkretouch
+
+import android.graphics.Bitmap
+import java.util.*
+
+class FilterThumbnailsManager {
+    private var filterThumbnails = ArrayList<FilterItem>(10)
+    private var processedThumbnails = ArrayList<FilterItem>(10)
+
+    fun addThumb(filterItem: FilterItem) {
+        filterThumbnails.add(filterItem)
+    }
+
+    fun processThumbs(): ArrayList<FilterItem> {
+        for (filterItem in filterThumbnails) {
+            filterItem.bitmap = filterItem.filter.processFilter(Bitmap.createBitmap(filterItem.bitmap))
+            processedThumbnails.add(filterItem)
+        }
+        return processedThumbnails
+    }
+
+    fun clearThumbs() {
+        filterThumbnails = ArrayList()
+        processedThumbnails = ArrayList()
+    }
+}
