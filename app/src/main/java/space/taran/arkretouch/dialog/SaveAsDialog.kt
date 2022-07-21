4059a095205fca4387a28a3b0ae2c7c1446f857b
Dinar Khakimov <85668474+mdrlzy@users.noreply.github.com>
2022-07-21 19:01:15 +0500

    #39: Adopted ark-filepicker and upgraded saving dialog to DialogFragment

--- app/src/main/java/space/taran/arkretouch/dialog/SaveAsDialog.kt
+++ app/src/main/java/space/taran/arkretouch/dialog/SaveAsDialog.kt
@@ -1,97 +1,182 @@
 package space.taran.arkretouch.dialog
 
-import androidx.appcompat.app.AlertDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.setFragmentResult
+import androidx.lifecycle.LifecycleOwner
+import by.kirich1409.viewbindingdelegate.viewBinding
 import com.simplemobiletools.commons.dialogs.ConfirmationDialog
-import com.simplemobiletools.commons.extensions.*
-import kotlinx.android.synthetic.main.dialog_save_as.view.*
+import com.simplemobiletools.commons.extensions.getDoesFilePathExist
+import com.simplemobiletools.commons.extensions.getFilenameFromPath
+import com.simplemobiletools.commons.extensions.getParentPath
+import com.simplemobiletools.commons.extensions.hideKeyboard
+import com.simplemobiletools.commons.extensions.humanizePath
+import com.simplemobiletools.commons.extensions.isAValidFilename
+import com.simplemobiletools.commons.extensions.toast
+import com.simplemobiletools.commons.extensions.value
+import space.taran.arkfilepicker.ArkFilePickerConfig
+import space.taran.arkfilepicker.ArkFilePickerFragment
+import space.taran.arkfilepicker.ArkFilePickerMode
+import space.taran.arkfilepicker.onArkPathPicked
 import space.taran.arkretouch.R
+import space.taran.arkretouch.databinding.DialogSaveAsBinding
 import space.taran.arkretouch.internal.BaseActivity
+import space.taran.arkretouch.utils.args
+import kotlin.io.path.Path
 
-class SaveAsDialog(
-    val activity: BaseActivity,
-    val folderPath: String?,
-    val path: String,
-    val appendFilename: Boolean,
-    val cancelCallback: (() -> Unit)? = null,
-    val callback: (savePath: String) -> Unit
-) {
+class SaveAsDialogFragment : DialogFragment(R.layout.dialog_save_as) {
+    private val binding by viewBinding(DialogSaveAsBinding::bind)
+    private var folderPath by args<String?>()
+    private var path by args<String>()
+    private var appendFilename by args<Boolean>()
+    private lateinit var filenameSuffix: String
 
-    private var filenameSuffix: String = if (appendFilename) "_1" else ""
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+    }
 
-    init {
-        var realPath = folderPath ?: path.getParentPath()
+    private fun initUI() = binding.apply {
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        val activity = requireActivity()
 
-        val view = activity.layoutInflater.inflate(R.layout.dialog_save_as, null).apply {
-            save_as_path.text = "${activity.humanizePath(realPath).trimEnd('/')}/"
+        var realPath = folderPath ?: path!!.getParentPath()
+        filenameSuffix = if (appendFilename!!) "_1" else ""
 
-            val fullName = path.getFilenameFromPath()
-            val dotAt = fullName.lastIndexOf(".")
-            var name = fullName
+        saveAsPath.text = "${activity.humanizePath(realPath).trimEnd('/')}/"
 
-            if (dotAt > 0) {
-                name = fullName.substring(0, dotAt)
-                val extension = fullName.substring(dotAt + 1)
-                save_as_extension.setText(extension)
-            }
+        val fullName = path!!.getFilenameFromPath()
+        val dotAt = fullName.lastIndexOf(".")
+        var name = fullName
 
-            overwrite.setOnCheckedChangeListener { _, isChecked ->
-                filenameSuffix = if (isChecked) "" else "_1"
-                save_as_name.setText(name + filenameSuffix)
-            }
+        if (dotAt > 0) {
+            name = fullName.substring(0, dotAt)
+            val extension = fullName.substring(dotAt + 1)
+            saveAsExtension.setText(extension)
+        }
 
-            save_as_name.setText(name + filenameSuffix)
-            save_as_path.setOnClickListener {
-                activity.hideKeyboard(save_as_path)
-                FilePickerDialog(activity, realPath, false, false, true, false) {
-                    save_as_path.text = activity.humanizePath(it)
-                    realPath = it
-                }
+        overwrite.setOnCheckedChangeListener { _, isChecked ->
+            filenameSuffix = if (isChecked) "" else "_1"
+            saveAsName.setText(name + filenameSuffix)
+        }
+
+        saveAsName.setText(name + filenameSuffix)
+        saveAsPath.setOnClickListener {
+            activity.hideKeyboard(saveAsPath)
+            ArkFilePickerFragment
+                .newInstance(getFilePickerConfig(realPath))
+                .show(childFragmentManager, null)
+
+            childFragmentManager.onArkPathPicked(activity) {
+                saveAsPath.text = activity.humanizePath(it.toString())
+                realPath = it.toString()
             }
         }
 
-        AlertDialog.Builder(activity)
-            .setPositiveButton(R.string.ok, null)
-            .setNegativeButton(R.string.cancel) { dialog, which -> cancelCallback?.invoke() }
-            .setOnCancelListener { cancelCallback?.invoke() }
-            .create().apply {
-                activity.setupDialogStuff(view, this, R.string.save_as) {
-                    showKeyboard(view.save_as_name)
-                    getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener {
-                        val filename = view.save_as_name.value
-                        val extension = view.save_as_extension.value
+        btnCancel.setOnClickListener {
+            dismiss()
+        }
 
-                        if (filename.isEmpty()) {
-                            activity.toast(R.string.filename_cannot_be_empty)
-                            return@setOnClickListener
-                        }
+        btnOk.setOnClickListener {
+            val filename = saveAsName.value
+            val extension = saveAsExtension.value
 
-                        if (extension.isEmpty()) {
-                            activity.toast(R.string.extension_cannot_be_empty)
-                            return@setOnClickListener
-                        }
-
-                        val newFilename = "$filename.$extension"
-                        val newPath = "${realPath.trimEnd('/')}/$newFilename"
-                        if (!newFilename.isAValidFilename()) {
-                            activity.toast(R.string.filename_invalid_characters)
-                            return@setOnClickListener
-                        }
-
-                        if (activity.getDoesFilePathExist(newPath) && !view.overwrite.isChecked) {
-                            val title = String.format(
-                                activity.getString(R.string.file_already_exists_overwrite),
-                                newFilename
-                            )
-                            ConfirmationDialog(activity, title) {
-                                callback(newPath)
-                                dismiss()
-                            }
-                        } else {
-                            callback(newPath)
-                            dismiss()
-                        }
-                    }
-                }
+            if (filename.isEmpty()) {
+                activity.toast(R.string.filename_cannot_be_empty)
+                return@setOnClickListener
             }
+
+            if (extension.isEmpty()) {
+                activity.toast(R.string.extension_cannot_be_empty)
+                return@setOnClickListener
+            }
+
+            val newFilename = "$filename.$extension"
+            val newPath = "${realPath.trimEnd('/')}/$newFilename"
+            if (!newFilename.isAValidFilename()) {
+                activity.toast(R.string.filename_invalid_characters)
+                return@setOnClickListener
+            }
+
+            if (activity.getDoesFilePathExist(newPath) && !overwrite.isChecked) {
+                val title = String.format(
+                    activity.getString(R.string.file_already_exists_overwrite),
+                    newFilename
+                )
+                ConfirmationDialog(activity, title) {
+                    setResult(newPath)
+                    dismiss()
+                }
+            } else {
+                setResult(newPath)
+                dismiss()
+            }
+        }
+    }
+
+    private fun setResult(path: String) = setFragmentResult(
+        SAVE_AS_REQUEST_KEY,
+        Bundle().apply {
+            putString(
+                PICKED_PATH_BUNDLE_KEY,
+                path
+            )
+        }
+    )
+
+    private fun getFilePickerConfig(initPath: String) = ArkFilePickerConfig(
+        initialPath = Path(initPath),
+        mode = ArkFilePickerMode.FOLDER,
+        titleStringId = R.string.pick_folder
+    )
+
+    companion object {
+        const val SAVE_AS_REQUEST_KEY = "saveAs"
+        const val PICKED_PATH_BUNDLE_KEY = "pickedPath"
+
+        fun newInstance(
+            folderPath: String?,
+            path: String,
+            appendFilename: Boolean,
+        ) = SaveAsDialogFragment().apply {
+            this.folderPath = folderPath
+            this.path = path
+            this.appendFilename = appendFilename
+        }
+
+        fun newInstanceAndShow(
+            activity: BaseActivity,
+            folderPath: String?,
+            path: String,
+            appendFilename: Boolean,
+            listener: (String) -> Unit
+        ) {
+            activity.supportFragmentManager.onSaveAsConfirmed(activity) {
+                listener(it)
+            }
+            newInstance(folderPath, path, appendFilename).show(
+                activity.supportFragmentManager,
+                null
+            )
+        }
     }
 }
+
+fun FragmentManager.onSaveAsConfirmed(
+    lifecycleOwner: LifecycleOwner,
+    listener: (String) -> Unit
+) {
+    setFragmentResultListener(
+        SaveAsDialogFragment.SAVE_AS_REQUEST_KEY,
+        lifecycleOwner
+    ) { _, bundle ->
+        listener(
+            bundle.getString(SaveAsDialogFragment.PICKED_PATH_BUNDLE_KEY)!!
+        )
+    }
+}
+
