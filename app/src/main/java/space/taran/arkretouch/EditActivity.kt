aa0e38a206213a4f00bc4722a3ea6bdc79429ec6
Parvin Pathan <pgpathan22@gmail.com>
2022-07-23 13:56:54 +0530

    Resolve ACRA crash with comment "ARK Retouch: saving an image"

--- app/src/main/java/space/taran/arkretouch/EditActivity.kt
+++ app/src/main/java/space/taran/arkretouch/EditActivity.kt
@@ -566,7 +566,7 @@ class EditActivity : BaseActivity(), CropImageView.OnCropImageCompleteListener {
                 saveUri =
                     Uri.fromFile(File("$internalStoragePath/${getCurrentFormattedDateTime()}.jpg"))
             }
-            if (bitmap!=null){
+            if (bitmap != null) {
                 if (saveUri.scheme == "file") {
                     SaveAsDialogFragment.newInstanceAndShow(this, savePath, saveUri.path!!, true) {
                         saveBitmapToFile(bitmap, it, true)
