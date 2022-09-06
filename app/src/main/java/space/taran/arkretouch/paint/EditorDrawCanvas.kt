382db168cdd1124e2d81fdab5478113de982f8d7
Parvin Pathan <pgpathan22@gmail.com>
2022-09-06 12:29:46 +0530

    Build fail issue resolved.

--- app/src/main/java/space/taran/arkretouch/paint/EditorDrawCanvas.kt
+++ app/src/main/java/space/taran/arkretouch/paint/EditorDrawCanvas.kt
@@ -57,8 +57,7 @@ class EditorDrawCanvas(context: Context, attrs: AttributeSet) :
     private var oldDist = 1f
     private val mid = PointF()
     private val start = PointF()
-    private var 
-    = false
+
     init {
         mColor = ContextCompat.getColor(context, R.color.color_primary)
         mPaint.apply {
